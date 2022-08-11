d76cb2d1f7fa349549ca4b4d04e959ed5e54bb1a
joao.albert <joao.alb@usp.br>
2022-08-11 09:30:20 -0300

    Adjusting file changing function

--- requeue_auto_run_hide.py
+++ requeue_auto_run_hide.py
@@ -5,11 +5,11 @@ import time
 rhPath = "/scratch/bingo/joao.barretos/hide_and_seek/hide-beam/run_hide.py" # run_hide -> horns
 arhPath = "/scratch/bingo/joao.barretos/hide_and_seek/HS_scripts/auto_run_hide.py" # auto_run_hide -> days
 #outPath = ""
-batchPath = "/scratch/bingo/joao.barretos/hide_and_seek/sbatches/sbatch_auto_hide.sh"
-squeue = "squeue -u joao.barretos | grep auto_hide"
+batchPath = "/scratch/bingo/joao.barretos/hide_and_seek/sbatches/sbatch_hide_zernike.srm"
+squeue = "squeue -u joao.barretos | grep hide"
 
 
-horns_i, horns_f = 3, 27 #0-27
+horns_i, horns_f = 4, 27 #0-27
 days_i, days_f = 1, 5 #1-30
 
 # this must match what bingo.py says
@@ -19,7 +19,7 @@ obs_month = "01"
 ############################################################
 
 #LAST_FILE_FMT = "bingo_tod_horn_{horn}_201801{dd:02d}_230000.h5"
-SLEEP_TIME = 60  # cyclic time to check if run is over (seconds)
+SLEEP_TIME = 30  # cyclic time to check if run is over (seconds)
 
 RH_LINE = "initial_horn, final_horn"
 ARH_LINE = "	initial_day, final_day"
@@ -27,18 +27,21 @@ ARH_LINE = "	initial_day, final_day"
 run = 1
 N = (horns_f-horns_i)*(days_f-days_i)
 
-sbatch = "sbatch {}".format(batchPath)
+sbatch = "echo vale"#"sbatch {}".format(batchPath)
 
 
 ############################################################
 
 def change_one_line(fpath, starts_with, change_to):
-	with open(fpath, "w") as fopen:
-		fline = fopen.readline()
-		if fline.startswith(starts_with):
-			fopen.write(change_to)
-		else:
-			fopen.write(fline)
+
+    with open(fpath, "r") as fopen:    
+        flines = fopen.readlines()
+        
+    with open(fpath, "w") as fopen:    
+        for fline in flines:
+            if fline.startswith(starts_with):
+                fline = change_to
+            fopen.write(fline)
 
 
 ############################################################
@@ -48,27 +51,28 @@ print("Start time at {}\n".format(start))
         
 for horn in range(horns_i, horns_f+1):
 
-	# Change run_hide file
-	rh_newline = RH_LINE + " = {}, {}\n".format(horn, horn+1)
-	change_one_line(rhPath, RH_LINE, rh_newline)
+    # Change run_hide file
+    rh_newline = RH_LINE + " = {}, {}\n".format(horn, horn+1)
+    change_one_line(rhPath, RH_LINE, rh_newline)
 
-	for day in range(days_i, days_f+1):
-	
+    for day in range(days_i, days_f+1):
+    
         print('\n\nRunning horn {} day {} ({}/{} run)'.format(horn, day, run, N))
         print("Run time: {}".format(time.ctime()))
         
         # Change auto_run_hide file
-        arh_newline = ARH_LINE + " = {}, {}".format(day, day+1)
+        arh_newline = ARH_LINE + " = {}, {}\n".format(day, day+1)
         change_one_line(arhPath, ARH_LINE, arh_newline)
         
         # Run sbatch
         os.system(sbatch)
+        time.sleep(SLEEP_TIME)
         
         # Keep waiting while batch is in the queue
         while os.popen(squeue).read()!="": 
             print('Still running...')
-        	time.sleep(SLEEP_TIME)
-        	
+            time.sleep(SLEEP_TIME)
+            
         print("End of run {}. Requeuing next...".format(run))
         run += 1
-            	
+                
